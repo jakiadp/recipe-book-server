ecb2e15a3ded43da9ebbfb18d65fc2671a3854d0
jakiadp <jakiadp@gmail.com>
2025-09-21 09:40:43 +0600

    add to get

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -31,12 +31,13 @@ async function run() {
     await client.connect();
 
 const recipesCollection = client.db('recipeDB').collection('recipes');
+
 app.get('/recipes',async(req, res) =>{
   const result = await recipesCollection.find().toArray();
   res.send(result);
 })
 
-
+ 
     app.post('/recipes', async(req, res)=>{
       const  newRecipe = req.body;
       console.log(newRecipe);
@@ -44,6 +45,13 @@ app.get('/recipes',async(req, res) =>{
       res.send(result)
     })
 
+app.delete('/recipes/:id', async(req, res)=> {
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)};
+  const result = await recipesCollection.deleteOne(query);
+  res.send(result);
+
+})
   
 
     
